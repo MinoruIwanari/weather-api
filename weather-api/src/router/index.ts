322a1db265df9c235e03cfc13b56d48dc1049cae
MinoruIwanari <iwanari.minoru.6dp@jp.nssol.nipponsteel.com>
2026-09-11 09:46:34 +0900

    4-1まで全部コミット

--- weather-api/src/router/index.ts
+++ weather-api/src/router/index.ts
@@ -1,23 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Vue3_3 from "../components/3-3.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      name: "3-3",
+      component: Vue3_3,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/4-1",
+      name: "4-1",
+      component: () => import("../components/4-1.vue"),
+    },
+    {
+      path: "/5-1",
+      name: "5-1",
+      component: () => import("../components/5-1.vue"),
+    },
+    {
+      path: "/6-1",
+      name: "6-1",
+      component: () => import("../components/6-1.vue"),
+    },
+    {
+      path: "/7-1",
+      name: "7-1",
+      component: () => import("../components/7-1.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
